9e34f56f926e60aa9c27f8d4957b8ca35c70352a
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-30 13:06:46 -0700

    Relationships among tables

--- server.py
+++ server.py
@@ -37,17 +37,51 @@ def register_form():
 
     return render_template("register_form.html")
 
-@app.route("/register", method= ["POST"])
+@app.route("/register", methods= ["POST"])
 def register_process():
-    input_email = request.form('email')
-    input_password = request.form('password')
+    input_email = request.form.get('email')
+    input_password = request.form.get('password')
 
-    if User.query.filter(User.email = input_email).first():
+    if User.query.filter_by(email = input_email).first():
         pass
     else:
         input_email = User(email = input_email, password = input_password)
+        db.session.add(input_email)
+        db.session.commit()
 
     return redirect("/")
+
+@app.route("/login")
+def login_user():
+
+    return render_template("login.html")
+
+@app.route("/login", methods=['POST'])
+def login_process():
+
+    login_email = request.form.get("email")
+    login_password = request.form.get("password")
+    print(login_password)
+
+    check_login = User.query.filter_by(email = login_email).first()
+
+    if check_login == None:
+        flash("Incorrect input! You are stuck on this page. There is no redirect.")
+        return render_template("login.html")
+
+    if (login_email == check_login.email) and (login_password == check_login.password):
+        flash("You were successfully logged in!")
+        return redirect('/')
+
+@app.route("/users/<int:user_id>")
+def show_user_info():
+
+    user_info = db.session.query.filter(User.user_id == user_id)
+    user_rating = user_info.ratings
+
+    for r in user_rating:
+        print(r.movie.title)
+
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
